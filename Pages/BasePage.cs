e93548a9b1eba48546ee45cbbea3bb6453d35486
Masindi Sekhwari <masindis@chasesoftware.co.za>
2026-07-22 21:43:26 +0200

    Initial commit: TRS.Web.Automation test project (NUnit)
    Selenium-based web automation test project using NUnit as the test framework.

---
+++ Pages/BasePage.cs
@@ -0,0 +1,19 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TRS.Web.Automation.Pages
+{
+    public class BasePage
+    {
+        protected IWebDriver Driver;
+
+        public BasePage(IWebDriver driver)
+        {
+            Driver = driver;
+        }
+    }
+}
---
+++ Tests/BaseTest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TRS.Web.Automation.Tests
+{
+    internal class BaseTest
+    {
+    }
+}
